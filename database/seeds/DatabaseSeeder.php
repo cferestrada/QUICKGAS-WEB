60a8bc471d40231a73828a9139a3cd15fb50013f
Caliopedanini <caliopedanini@gmail.com>
2018-01-18 15:39:11 -0600

    migration de rol,empresa,sucursal,persona,tipoproducto,trabajador,vehiculo,pais,estado,municipio / seeds empresa,estado,municipio,paises,persona,rol,sucursal,tipoproducto

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,35 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        /*
+        $this->truncateTables([
+            'rol'
+        ]);
+        */
+
         // $this->call(UsersTableSeeder::class);
+
+        /*
+        $this->call(RolSeeder::class);
+        $this->call(EmpresaSeeder::class);
+
+        */
+        
+        $this->call(SucursalSeeder::class);
+        $this->call(PaisSeeder::class);
+        $this->call(EstadoSeeder::class);
+
+        $this->call(SucursalSeeder::class);
+        $this->call(SucursalSeeder::class);
+        
     }
+
+  /*  protected function truncateTables(array $tables){
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
+        foreach($tables as $table){
+            DB::table('$table')->truncate();
+        }
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1;');
+    }
+    */
 }
